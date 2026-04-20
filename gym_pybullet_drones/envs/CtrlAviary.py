90a68f72b1ec3c9bae7d98f7ffbe8cb08d9cae10
Craig White <gerzytet@gmail.com>
2026-04-19 20:23:20 -0400

    Standardize some settings Added final results files

--- gym_pybullet_drones/envs/CtrlAviary.py
+++ gym_pybullet_drones/envs/CtrlAviary.py
@@ -54,6 +54,7 @@ class CtrlAviary(BaseAviary):
             Whether to draw the drones' axes and the GUI RPMs sliders.
 
         """
+        self.EPISODE_LEN_SEC = 10
         super().__init__(drone_model=drone_model,
                          num_drones=num_drones,
                          neighbourhood_radius=neighbourhood_radius,
